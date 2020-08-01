b2ced7a236f11ccc20f1a644541423392821ba6b
calvinlinardy <calvin.linardy@yahoo.com>
2020-08-02 00:13:37 +0800

    Adding Everything

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    [SerializeField] AudioClip hitSound = null;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            AudioSource.PlayClipAtPoint(hitSound, Camera.main.transform.position);
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
